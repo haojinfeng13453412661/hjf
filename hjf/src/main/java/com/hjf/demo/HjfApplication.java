af3ffa15a9fec86a64ea3987c70b93602e5e2a3d
hjf <13453412661@163.com>
2019-11-26 10:37:13 +0800

    第一次提交

---
+++ hjf/src/main/java/com/hjf/demo/HjfApplication.java
@@ -0,0 +1,21 @@
+package com.hjf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HjfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjfApplication.class, args);
+    }
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "Hello World!";
+    }
+
+}
---
+++ hjf/src/test/java/com/hjf/demo/HjfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
